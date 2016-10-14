b426cb3d82e0144c5cf92b9b8fe06a999432d0ea
raknahsa <raknahsa@gmail.com>
2016-10-14 16:38:50 -0400

    Add checkBase and checkSign function
    Add functions to the check the base and the sign of the inputs. Also add
    information about the program and the functions in the comments

--- calc.c
+++ calc.c
@@ -1,9 +1,16 @@
+/*
+ * Anuraag Shankar
+ * Calc.c Program
+ * CS211
+ */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
 
-int checkOperation(char * input){
+int checkOperation(char * input){//Check the operation
     if(input[0] == '+'){
         return 1;
     }
@@ -18,14 +25,81 @@ int checkOperation(char * input){
     }
 }
 
+int checkSign(char * input){//Check the sign
+    if(input[0] == '-'){
+        return 0;
+    } 
+    else{
+        return 1;
+    }
+}
+
+int checkBase(char * input){ //Check the base
+    if(input[0] == '-'){
+        if(input[1] == 'b'){
+            return 1;
+        }
+        else if(input[1] == 'd'){
+            return 10;
+        }
+        else if(input[1] == 'o'){
+            return 8;
+        }
+        else if(input [1] == 'x'){
+            return 16;
+        }
+    }
+    else if(input[0] == 'b'){
+        return 1;
+    }
+    else if(input[0] == 'd'){
+        return 10;
+    }
+    else if(input[0] == 'o'){
+        return 8;
+    }
+    else if(input [0] == 'x'){
+        return 16;
+    }
+    else{
+        return 666;
+    }
+
+    return 666;
+}
 
 int main(int argc, char **argv){
     int operation = 0;
-    operation = checkOperation(argv[1]);
+    int sign1 = 0;
+    int base1 = 0;
+    int sign2 = 0;
+    int base2 = 0;
+
+    operation = checkOperation(argv[1]);    
     if(operation == 666){
         printf("Error: Invalid operation '%s'", argv[1]);
+        return 0;
     }
     //printf("%d , %s\n", operation, argv[1]);
+    sign1 = checkSign(argv[2]);
+    base1 = checkBase(argv[2]);
+    if(base1 == 666){
+        printf("Error: Invalid input '%s'", argv[2]);
+        return 0;
+    }
+
+    //printf("%d , %s\n", sign1, argv[2]);
+    //printf("%d , %s\n", base1, argv[2]);
+    
+    sign2 = checkSign(argv[3]);
+    base2 = checkBase(argv[3]);
+    if(base2 == 666){
+        printf("Error: Invalid input '%s'", argv[3]);
+        return 0;
+    }
+    
+    //printf("%d , %s\n", sign2, argv[3]); 
+    //printf("%d , %s\n", base2, argv[3]);
     
     return 0;
 }
