43855bbded9b33f8f075220f490cbd62c00e6b2f
raknahsa <raknahsa@gmail.com>
2016-10-14 18:02:50 -0400

    Add binary conversion functions
    Add functions to convert ascii number to binary int and reverse.

--- calc.c
+++ calc.c
@@ -4,7 +4,6 @@
  * CS211
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -27,10 +26,10 @@ int checkOperation(char * input){//Check the operation
 
 int checkSign(char * input){//Check the sign
     if(input[0] == '-'){
-        return 0;
+        return 1;
     } 
     else{
-        return 1;
+        return 0;
     }
 }
 
@@ -50,7 +49,7 @@ int checkBase(char * input){ //Check the base
         }
     }
     else if(input[0] == 'b'){
-        return 1;
+        return 2;
     }
     else if(input[0] == 'd'){
         return 10;
@@ -68,12 +67,60 @@ int checkBase(char * input){ //Check the base
     return 666;
 }
 
+char * intToBinaryASCII(unsigned int x, char * output){
+    unsigned int i, n = 0;
+    char * cstr = malloc(3);
+    output[0] = '\0';
+    for(i = 0; i<32; i++){
+        n = x>>31;
+        cstr[0] = '0' + n;
+        cstr[1] = '\0';
+        strcat(output,cstr);
+        x = x<< 1;
+    }
+
+    i = 0;
+    while(output[i] == '0'){
+        i++;
+    }
+
+    cstr = 0;
+    free(cstr);
+   // printf("test1");
+    return output + i;
+}
+
+unsigned int binASCIIToInt(char * str){
+    int i, value = 0;
+    for(i = 0; str[i] != '\0'; i++){
+   //   printf("run : %d\n", i);
+        switch(str[i])
+        {
+            case '0':
+                value = (value<<1);
+                //printf("%d\n", value);
+                break;
+            case '1':
+                value = (value<<1)+1;
+                //printf("%d\n", value);
+                break;
+            default:
+                printf("Error: Something went wrong binToASCII");
+                return 0;
+        }
+    }
+    //printf("test0");
+    return value;
+}
+
 int main(int argc, char **argv){
     int operation = 0;
     int sign1 = 0;
     int base1 = 0;
     int sign2 = 0;
     int base2 = 0;
+    unsigned int value1 = 0;
+    unsigned int value2 = 0;
 
     operation = checkOperation(argv[1]);    
     if(operation == 666){
@@ -98,6 +145,22 @@ int main(int argc, char **argv){
         return 0;
     }
     
+    switch(base1){
+        case(2):
+            value1 = binASCIIToInt(argv[2] + 1 + sign1);
+            char * output = malloc(strlen(argv[2]));
+            printf("b%s\n", intToBinaryASCII(value1,output)); 
+        case(8):
+            //to be filled
+            break;
+        case(10):
+            //to be filled
+            break;
+        case(16):
+            //to be filled
+            break;
+    }
+
     //printf("%d , %s\n", sign2, argv[3]); 
     //printf("%d , %s\n", base2, argv[3]);
     
