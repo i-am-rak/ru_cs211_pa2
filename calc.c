0e0d01c923fe5207a4f5cf4ac3292134aef38a76
raknahsa <raknahsa@gmail.com>
2016-10-14 16:01:06 -0400

    Add Check Operation Function
    Add a check operation function to see which operation is being performed
    by the program.Program is then tested in the main.

--- calc.c
+++ calc.c
@@ -3,9 +3,27 @@
 #include <ctype.h>
 #include <string.h>
 
+int checkOperation(char * input){
+    if(input[0] == '+'){
+        return 1;
+    }
+    else if(input[0] == '-'){
+        return 2;
+    }
+    else if(input[0] == '*'){
+        return 3;
+    }
+    else{
+        return 666;
+    }
+}
+
 
 int main(int argc, char **argv){
-
-  return 0;
+    int operation = 0;
+    operation = checkOperation(argv[1]);
+    //printf("%d , %s\n", operation, argv[1]);
+    
+    return 0;
 }
 
