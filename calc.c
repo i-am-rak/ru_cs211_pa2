00d2f319d73203e793bad607c6826c3b64a30f2f
raknahsa <raknahsa@gmail.com>
2016-10-18 20:24:15 -0400

    Add generic functions
    Add a generic int to ascii function and ascii to int function.

--- calc.c
+++ calc.c
@@ -67,7 +67,7 @@ int checkBase(char * input){ //Check the base
     return 666;
 }
 
-char * intToBinaryASCII(unsigned int x, char * output){
+char * intToBinaryASCII(unsigned int x, char * output){//convert int to ascii
     unsigned int i, n = 0;
     char * cstr = malloc(3);
     output[0] = '\0';
@@ -113,14 +113,110 @@ unsigned int binASCIIToInt(char * str){
     return value;
 }
 
+char * intToASCII(int x, char * output,int base,int sign){
+    //char * octChar = "01234567";
+    if(sign == 1){
+        x = x*-1;
+    }
+    int i,n = 0;
+    char * cstr = malloc(32);
+    char c = 0;
+    char * output2 = malloc(32);
+    output2[0] = '\0';
+    while(x!=0){
+        cstr[0]= (x%base) + '0';
+        if(x%base > 9){
+            n = base - x%base;
+            cstr[0] = 'G' - n; 
+        }
+        cstr[1] = '\0';
+        //printf("-%s-\n", cstr);
+        strcat(output2,cstr);
+        x /= base;    
+    }
+
+    i = 0;
+    while(i < strlen(output2)){
+        output[i] = output2[strlen(output2)-i-1];
+        //printf("-%c-\n", output[i]);
+        i++;
+    }
+
+    cstr = 0;
+    free(cstr);
+    
+    return output;
+}
+
+int ASCIIToInt(char * str,int base, int sign) {
+
+    int output = 0;
+    int i = 0;
+    while(str[i] != '\0'){
+        if(str[i] >= 'a' && str[i] <= 'f'){
+            output = output*base + str[i] - 'a' + 10;
+        }
+        else if(str[i] >= 'A' && str[i] <= 'F'){
+            output = output*base + str[i] - 'A' + 10;
+        }
+        else
+            output = output*base + str[i] - '0';
+
+        i++;
+    }
+
+    if(sign == 1){
+        output = output *-1;
+    }
+
+    return output;
+}
+
+unsigned int octASCIIToInt(char * str){
+    int i, value = 0;
+    for(i = 0; str[i]!= '\0'; i++){
+//        printf("run %d\n", i);
+        switch(str[i]){
+            case '0': value = (value<<3); break;
+            case '4': value = (value<<1) + 1;
+                      value = (value<<2);
+                      break;
+            case '2': value = (value<<2) + 1;
+                      value = (value<<1);
+                      break;
+            case '6': value = (value<<1) + 1;
+                      value = (value<<1) + 1;
+                      value = (value<<1);
+                      break;
+            case '1': value = (value<<3) + 1;
+                      break;
+            case '5': value = (value<<1)+1;
+                      value = (value<<2)+1;
+                      break;
+            case '3': value = (value<<2)+1;
+                      value = (value<<1)+1;
+                      break;
+            case '7': value = (value<<1) +1;
+                      value = (value<<1) +1;
+                      value = (value<<1) +1;
+                      break;
+            default: printf("not happy %c \n",  str[i]); return 1;
+        }
+    }
+  //  printf("value %d\n", value);
+    return value;
+}
+
 int main(int argc, char **argv){
     int operation = 0;
     int sign1 = 0;
     int base1 = 0;
     int sign2 = 0;
     int base2 = 0;
-    unsigned int value1 = 0;
-    unsigned int value2 = 0;
+    int value1 = 0;
+    int value2 = 0;
+    int output = 0;
+    int outputSign = 0;
 
     operation = checkOperation(argv[1]);    
     if(operation == 666){
@@ -147,19 +243,86 @@ int main(int argc, char **argv){
     
     switch(base1){
         case(2):
-            value1 = binASCIIToInt(argv[2] + 1 + sign1);
-            char * output = malloc(strlen(argv[2]));
-            printf("b%s\n", intToBinaryASCII(value1,output)); 
+            value1 = ASCIIToInt(argv[2] + 1 + sign1,base1,sign1);
+            //char * output = malloc(strlen(argv[2]));
+            //printf("b%s\n", intToASCII(value1,output,base1,sign1));
+            break; 
         case(8):
             //to be filled
+            value1 = ASCIIToInt(argv[2] + 1 + sign1,base1,sign1);
+            //char * output2 = malloc(strlen(argv[2]));
+            //printf("o%s\n", intToASCII(value1,output2,base1,sign1)); 
             break;
         case(10):
             //to be filled
+            value1 = ASCIIToInt(argv[2] + 1 + sign1,base1,sign1);
+            //char * output3 = malloc(strlen(argv[2]));
+            //printf("o%s\n", intToASCII(value1,output3,base1,sign1)); 
             break;
         case(16):
             //to be filled
+            value1 = ASCIIToInt(argv[2] + 1 + sign1,base1,sign1);
+            //char * output4 = malloc(strlen(argv[2]));
+            //printf("o%s\n", intToASCII(value1,output4,base1,sign1));   
             break;
     }
+
+     switch(base2){
+        case(2):
+            value2 = ASCIIToInt(argv[3] + 1 + sign2,base2,sign2);
+            //char * output = malloc(strlen(argv[2]));
+            //printf("b%s\n", intToASCII(value2,output,base2,sign2));
+            break; 
+        case(8):
+            //to be filled
+            value2 = ASCIIToInt(argv[3] + 1 + sign2,base2,sign2);
+            //char * output2 = malloc(strlen(argv[2]));
+            //printf("o%s\n", intToASCII(value1,output2,base1,sign1)); 
+            break;
+        case(10):
+            //to be filled
+            value2 = ASCIIToInt(argv[3] + 1 + sign2,base2,sign2);
+            //char * output3 = malloc(strlen(argv[2]));
+            //printf("o%s\n", intToASCII(value1,output3,base1,sign1)); 
+            break;
+        case(16):
+            //to be filled
+            value2 = ASCIIToInt(argv[3] + 1 + sign2,base2,sign2);
+            //char * output4 = malloc(strlen(argv[2]));
+            //printf("o%s\n", intToASCII(value1,output4,base1,sign1));   
+            break;
+    }
+
+    switch(operation){
+        case 1:
+            output = value1 + value2;
+            if(output < 0){
+                outputSign = 1;
+            }
+            break;
+        case 2:
+            output = value1 - value2;
+            if(output < 0){
+                outputSign = 1;
+            }
+            break;
+         case 3:
+            output = value1 * value2;
+            if(output < 0) {
+                outputSign = 1;
+            }
+            break;
+    }
+
+
+    char * outputStr = malloc(35);
+    char baseChar = argv[4][0];
+    int base = checkBase(argv[4]);
+    char signChar = 0;
+    if(outputSign == 1){
+        signChar = '-';         
+    }
+    printf("%c%c%s\n", signChar,baseChar,intToASCII(output,outputStr,base,outputSign)); 
 
     //printf("%d , %s\n", sign2, argv[3]); 
     //printf("%d , %s\n", base2, argv[3]);
